84797028d8b11053ca34cb79bafbf76afb3e2b2f
Conor Costain <conorcostain@gmail.com>
2017-12-13 10:48:12 +0000

    Very Very Basics
    -Added Camera following
    -Added constant forward motion to player

---
+++ LaneSwitcher/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Vector3 Offset = new Vector3();
+
+	private GameObject player;
+	
+	// Use this for initialization
+	private void Awake () {
+
+		player = FindObjectsOfType<GameObject>().Where(g => g.tag == "Player").FirstOrDefault();
+
+	}
+
+	private void FixedUpdate()
+	{
+		transform.position = player.transform.position + Offset;
+	}
+
+
+}
---
+++ LaneSwitcher/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	public float transistionTime = 0.1f;
+
+
+	private void FixedUpdate()
+	{
+		transform.position += Vector3.forward * 5f * Time.fixedDeltaTime;
+	}
+}
