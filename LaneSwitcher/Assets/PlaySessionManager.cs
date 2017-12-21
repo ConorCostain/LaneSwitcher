f946580a7a7e5822718d6259e26d35f49799502f
Conor Costain <conorcostain@gmail.com>
2017-12-21 11:59:08 +0000

    Got working collisions
    -Tracks score
    -Detects collisions between objects and obstacles

---
+++ LaneSwitcher/Assets/PlaySessionManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySessionManager : MonoBehaviour {
+	public static PlaySessionManager ins;
+	public float score = 0f;
+	public int obstaclesHit = 0;
+	public int objectivesHit = 0;
+	public float distanceTravelled = 0f;
+
+	private void Awake()
+	{
+		if(ins == null)
+		{
+			ins = this;
+		}
+		else if (ins != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+
+	private void Update()
+	{
+		updateScore();
+	}
+
+	private void FixedUpdate()
+	{
+		distanceTravelled = FindObjectOfType<PlayerMovement>().transform.position.z;
+	}
+
+	private void updateScore()
+	{
+		score = (distanceTravelled * 10) + (objectivesHit * 100) - (obstaclesHit * 50);
+		Debug.Log(((int)score).ToString());
+	}
+
+	
+
+}
---
+++ LaneSwitcher/Assets/PlayerCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+	private float score = 0f;
+
+
+
+	private void OnTriggerEnter(Collider trigger)
+	{
+		if(trigger.tag == "Obstacle")
+		{
+			Obstacle(trigger);
+		}
+		else if (trigger.tag == "Objective")
+		{
+			Objective(trigger);
+		}
+	}
+
+	private void Obstacle(Collider trigger)
+	{
+		PlaySessionManager.ins.obstaclesHit++;
+	}
+
+	private void Objective(Collider trigger)
+	{
+		PlaySessionManager.ins.objectivesHit++;
+	}
+}
